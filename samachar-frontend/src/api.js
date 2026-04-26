b700c08a2ceff23fcd720a3e8d4e0b8b18486a08
Parth Gupta <guptaparth482@gmail.com>
2026-04-26 13:29:09 +0530

    fix date filter bug

--- samachar-frontend/src/api.js
+++ samachar-frontend/src/api.js
@@ -1,9 +1,9 @@
 //Single place for all API calls
 import axios from 'axios';
 
-// const BASE = 'https://samachar-api.onrender.com';  // your Render URL
+const BASE = 'https://samachar-api.onrender.com';  // your Render URL
 
-const BASE = 'http://localhost:8000';
+// const BASE = 'http://localhost:8000';
 export const getRecent = () => {
   return axios.get(`${BASE}/api/news/today`);
 };
